9d0bbcf386e9de0e287d85412ee1ab6f01c08d66
Giovana Ribeiro de Francisco <gi.ribeirodefrancisco@gmail.com>
2023-12-07 12:30:34 -0300

    Initial commit

---
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent {
+
+  nomePortal: string; 
+
+  cursos: string[] = ['Java', 'Ext JS', 'Angular']; 
+
+  constructor(){
+    this.nomePortal = 'http://loiane.training';
+    
+    for(let i = 0; i < this.cursos.length; i++){
+      let curso = this.cursos[i];
+    }
+  }
+}
